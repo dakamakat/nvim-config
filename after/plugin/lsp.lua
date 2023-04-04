167ed71909987ad893bd993bc0962557a98baeb3
dakamakat <gazon-ozon@yandex.ru>
2023-04-04 12:06:54 +0200

    feat(nvim) add cmp features / configure cmp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,21 +1,36 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require('lsp-zero').preset({
+  manage_nvim_cmp = {
+    set_sources = 'recommended'
+  }
+})
+
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true
+    }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+  }),
+})
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
 
 -- Fix Undefined global 'vim'
 lsp.configure('lua_ls', {
--- lua/dakamakat/packer.lua
+++ lua/dakamakat/packer.lua
@@ -19,7 +19,7 @@ return require('packer').startup(function(use)
     use 'xiyaowong/transparent.nvim'
 
     -- end of visual
-    
+
     use 'wbthomason/packer.nvim'
 
     use {
@@ -34,6 +34,9 @@ return require('packer').startup(function(use)
 
     use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
 
+    use 'windwp/nvim-autopairs'
+
+    use 'windwp/nvim-ts-autotag'
 
     use('nvim-lua/plenary.nvim')
 
@@ -57,7 +60,7 @@ return require('packer').startup(function(use)
             -- LSP Support
             { 'neovim/nvim-lspconfig' }, -- Required
             {
-                               -- Optional
+                -- Optional
                 'williamboman/mason.nvim',
                 run = function()
                     pcall(vim.cmd, 'MasonUpdate')
@@ -66,9 +69,13 @@ return require('packer').startup(function(use)
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' }, -- Required
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'L3MON4D3/LuaSnip' },         -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Optional
         }
     }
 end)
