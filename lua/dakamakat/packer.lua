f4a90b53436c0583395c20a57b43c9d8da7c420b
dakamakat <gazon-ozon@yandex.ru>
2023-04-16 00:03:35 +0200

    fix (packer) make packer opt

--- lua/dakamakat/packer.lua
+++ lua/dakamakat/packer.lua
@@ -1,9 +1,12 @@
 --This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
+
+    use {"wbthomason/packer.nvim", opt = true}
+
     -- Visual
 
     -- use { "ellisonleao/gruvbox.nvim" }
