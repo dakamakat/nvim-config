cd7027551e9050a11e39f0b1b1049b50603d785e
dakamakat <gazon-ozon@yandex.ru>
2023-10-10 11:56:20 +0300

    fix(keymaps): fix keymaps for debugging

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -172,7 +172,7 @@ end
 
 vim.keymap.set('n', '<F5>', require 'dap'.continue)
 vim.keymap.set('n', '<F10>', require 'dap'.step_over)
-vim.keymap.set('n', '<F3>', require 'dap'.step_into)
+vim.keymap.set('n', '<F7>', require 'dap'.step_into)
 vim.keymap.set('n', '<F12>', require 'dap'.step_out)
 vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint)
 
