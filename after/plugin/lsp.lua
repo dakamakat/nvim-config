74b7376748b1fa11571b912fc77fd52480815a37
dakamakat <gazon-ozon@yandex.ru>
2023-12-22 16:42:20 +0100

    feat(lsp): add csharp_ls to mason

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,7 +10,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = { 'tsserver', 'rust_analyzer' },
+    ensure_installed = { 'tsserver', 'rust_analyzer', 'csharp_ls' },
     handlers = {
         lsp_zero.default_setup,
         lua_ls = function()
