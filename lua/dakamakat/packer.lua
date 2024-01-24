364a8b0cadb3343ed3711a3d4f28bafceb1933bd
dakamakat <gazon-ozon@yandex.ru>
2024-01-24 11:30:26 +0100

    add

--- lua/dakamakat/packer.lua
+++ lua/dakamakat/packer.lua
@@ -83,6 +83,18 @@ return require('packer').startup(function(use)
             { "theHamsta/nvim-dap-virtual-text" }
         } }
 
+    use {
+        "folke/noice.nvim",
+        requires = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    }
+
 
     -- -- Project Switcher Config
     use {
