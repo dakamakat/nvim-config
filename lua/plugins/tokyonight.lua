ba21e495591dc259f57068237fbd2fd97cad3d0e
dakamakat <gazon-ozon@yandex.ru>
2024-01-25 14:39:25 +0100

    fix colorscheme warning

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,7 +2,7 @@ return {
     'folke/tokyonight.nvim',
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    options = {
+    opts = {
         ---@diagnostic disable: undefined-doc-name
         -- your configuration comes here
         -- or leave it empty to use the default settings
