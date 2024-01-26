08b11e70b1266bb29285fd16b8aced479dae0e3a
dakamakat <gazon-ozon@yandex.ru>
2024-01-26 10:06:15 +0100

    fix treesitter error / minor config update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,7 +11,8 @@ return {
                 "javascript",
                 "typescript",
                 "c_sharp",
-                "lua",
+                "sql",
+                "yaml",
                 "rust",
                 "vim",
                 "regex",
@@ -22,30 +23,6 @@ return {
 
             highlight = { enable = true },
             indent = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = '<cr>',
-                    node_incremental = '<c-s>',
-                    scope_incremental = '<c-s>',
-                    node_decremental = '<bs>',
-                },
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true, -- automatically jump forward to textobj, similar to targets.vim
-                    keymaps = {
-                        -- you can use the capture groups defined in textobjects.scm
-                        ['aa'] = '@parameter.outer',
-                        ['ia'] = '@parameter.inner',
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner',
-                    },
-                },
-            },
             autotag = {
                 enable = true,
             },
