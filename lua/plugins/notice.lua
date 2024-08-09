f560f644c997bc6fc5dca8411a4de1cbb93fe47e
dakamakat <gazon-ozon@yandex.ru>
2024-08-09 15:05:20 +0200

    configure noice styling

--- lua/plugins/notice.lua
+++ lua/plugins/notice.lua
@@ -9,9 +9,20 @@ return
     dependencies = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        -- "rcarriga/nvim-notify",
     },
     config = function()
         require("noice").setup({
+            views = {
+                mini = {
+                    win_options = {
+                        winblend = 0
+                    }
+                },
+            },
             lsp = {
                 -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
                 override = {
@@ -25,7 +36,7 @@ return
                 bottom_search = true,         -- use a classic bottom cmdline for search
                 command_palette = true,       -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false,            -- enables an input dialog for inc-rename.nvim
+                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
                 lsp_doc_border = true,        -- add a border to hover docs and signature help
             },
             routes = {
@@ -36,6 +47,13 @@ return
                         find = 'записано',
                     },
                     opts = { skip = true },
+                },
+                {
+                    filter = {
+                        event = "notify",
+                        find = "No information available",
+                    },
+                    opts = { skip = true }
                 }
             },
         })
